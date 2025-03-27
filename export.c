9fea0e617932e0be884c7e0824d324226787344f
Liviana Dei Svaldi <liviana.deisvaldi@gmail.com>
2025-03-27 19:39:39 +0100

    export improved

--- export.c
+++ export.c
@@ -14,11 +14,13 @@ void sort_array(char **env)
 	char	*temp;
 	int		n;
 	int		b;
+	int		i;
 
 	n = 0;
 	while (env[n])
 	{
 		b = n + 1;
+		i = 0;
 		while (env[b])
 		{
 			if (ft_strncmp(env[n], env[b], ft_strlen(env[b])) > 0)
@@ -30,13 +32,34 @@ void sort_array(char **env)
 			}
 			b++;
 		}
-		printf("declare -x %s\n", env[n]);
+		printf("declare -x ");
+		while (env[n][i] != '=' && env[n][i] != '\0')
+		{
+			 printf("%c", env[n][i]);
+			 i++;
+		}
+		if (env[n][i] == '=')
+		{
+			printf("=\"%s\"\n", env[n] + i + 1);
+		}
 		n++;
 	}
 }
 
 void	ft_export(char **env, char **var)
 {
+	int		n;
+	char	**copy;
+
 	(void) var;
-	sort_array(env);
+	copy = calloc(1024, sizeof(char *));
+	n = 0;
+	while (env[n])
+	{
+		copy[n] = ft_strdup(env[n]);
+		n++;
+	}
+	copy[n] = NULL;
+	sort_array(copy);
+	free_array(copy);
 }
