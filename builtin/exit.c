766bcf3abe0a6dcd428141b662cf5fb712dcfd8c
Liviana Dei Svaldi <liviana.deisvaldi@gmail.com>
2025-04-20 19:08:14 +0200

    builtin directory

---
+++ builtin/exit.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldei-sva <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 15:35:53 by ldei-sva          #+#    #+#             */
+/*   Updated: 2025/04/11 15:35:58 by ldei-sva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	check(char *str)
+{
+	long long	number;
+	int			i;
+
+	number = 0;
+	i = 0;
+	if (!str)
+		return (0);
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i++])
+	{
+		if (str[i - 1] < '0' || str[i - 1] > '9')
+			return (0);
+		number = (number * 10) + (str[i - 1] - '0');
+	}
+	return (1);
+}
+
+void	ft_exit(char **exit_status)
+{
+	if (!(*exit_status))
+	{
+		ft_putstr_fd("exit\n", 2);
+		exit(0);
+	}
+	if (check(*exit_status) == 0)
+	{
+		printf("exit\n");
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(*exit_status, 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
+		exit(2);
+	}
+	if (*(exit_status + 1) != NULL)
+	{
+		printf("exit\n");
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		exit(1);
+	}
+	printf("exit\n");
+	exit(ft_atoi(*exit_status));
+}
