fe56ec0eb42c0e74ffdaa0de53565fb64287a4bf
Riccardo Emanuele Todaro <rtodaro@e4r10p2.42roma.it>
2025-04-23 16:23:19 +0200

    ORGANIZATION: norminetted signal_handlers.c

--- signal_handlers.c
+++ signal_handlers.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   signal_handlers.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldei-sva <ldei-sva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/19 16:03:41 by ldei-sva          #+#    #+#             */
-/*   Updated: 2025/04/19 17:17:03 by ldei-sva         ###   ########.fr       */
+/*   Created: 2025/04/23 16:21:10 by rtodaro           #+#    #+#             */
+/*   Updated: 2025/04/23 16:21:17 by rtodaro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    sigquit_handler(int signum)
+void	sigquit_handler(int signum)
 {
-     (void)signum;
-     signal_manager(SIGQUIT, SIG_IGN);
-     g_last_signal = SIGQUIT;
-     kill(0, SIGQUIT);
-     write(STDOUT_FILENO, "Quit\n", 5);
+	(void)signum;
+	signal_manager(SIGQUIT, SIG_IGN);
+	g_last_signal = SIGQUIT;
+	kill(0, SIGQUIT);
+	write(STDOUT_FILENO, "Quit\n", 5);
 }
 
-void sigint_handler(int signum)
+void	sigint_handler(int signum)
 {
-        extern  unsigned        long    rl_readline_state;
+	extern unsigned long	rl_readline_state;
 
-        (void)signum;
-        write(STDOUT_FILENO, "\n", 1);
-        rl_replace_line("", 0);
-        rl_on_new_line();
-        if (rl_readline_state & (RL_STATE_READCMD))
-                rl_redisplay();
-        g_last_signal = SIGINT;
+	(void)signum;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	if (rl_readline_state & (RL_STATE_READCMD))
+		rl_redisplay();
+	g_last_signal = SIGINT;
 }
 
 void	setup_signal_handlers(void)
@@ -49,20 +49,20 @@ void	setup_signal_handlers(void)
 	sigaction(SIGQUIT, &sq, NULL);
 }
 
-void    heredoc_handler(int signum)
+void	heredoc_handler(int signum)
 {
-        (void)signum;
-        rl_on_new_line();
-        g_last_signal = SIGINT;
-        exit(1);
+	(void)signum;
+	rl_on_new_line();
+	g_last_signal = SIGINT;
+	exit(1);
 }
 
-void     signal_manager(int signum, void (*handler)(int signum))
+void	signal_manager(int signum, void (*handler)(int signum))
 {
-        struct sigaction        sa;
+	struct sigaction	sa;
 
-        sa.sa_handler = handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = SA_RESTART;
-        sigaction(signum, &sa, NULL);
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(signum, &sa, NULL);
 }
